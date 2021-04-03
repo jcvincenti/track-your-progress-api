ed502325902f6d2399d022adf3c11a4e9ebc58a1
Juan Cruz Vincenti <juancruzvincenti@gmail.com>
2021-04-03 14:21:30 -0300

    Install cors package * Add cors middleware to index

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 
 const mongo = require('./mongo');
 const app = express();
 const universidadController = require('./controllers/Universidad');
+const carreraController = require('./controllers/Carrera');
 
 mongo.connect();
 
+app.use(cors());
+
 app.use('/api/universidades', universidadController);
+app.use('/api/carreras', carreraController);
 
 app.listen(process.env.PORT);
